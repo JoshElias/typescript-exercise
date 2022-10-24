d21fe2b5d697f36060176e8a224cd6870d07e4bd
Josh Elias <code.5jzcm@simplelogin.com>
2022-10-24 11:43:33 -0400

    add top-level async

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,10 +1,6 @@
 import fetch, { Response } from 'node-fetch'
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
-
-const run = async () => {
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-};
-run();
+const res = await fetch(url);
+const data = await res.json();
+console.log(data);

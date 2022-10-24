465219bc62900c34f05e90541ec8143ac8d9df4e
Josh Elias <code.5jzcm@simplelogin.com>
2022-10-24 11:52:41 -0400

    add todo type

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,6 +1,17 @@
 import fetch, { Response } from 'node-fetch'
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 const res = await fetch(url);
-const data = await res.json();
-console.log(data);
+const todo: Todo = await res.json() as Todo;
+const { id, title, completed } = todo;
+console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished: ${completed}
+`);

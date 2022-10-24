7e15daacce2111be01caa38b053fe7a35a17d642
Josh Elias <code.5jzcm@simplelogin.com>
2022-10-24 10:33:21 -0400

    add typescript to build

---
+++ fetchjson/index.ts
@@ -0,0 +1,10 @@
+import fetch from "node-fetch";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+const run = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+};
+run();

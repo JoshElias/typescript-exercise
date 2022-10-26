79fb0ef4e8abe103ca2decf9d88ac1c7b036f036
Josh Elias <code.5jzcm@simplelogin.com>
2022-10-26 10:55:36 -0400

    add initial bubble sort implimentation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,24 @@
-console.log('hi there');
+class Sorter {
+    constructor(public collection: number[]) {}
 
-const logSomething = () => {
-    console.log("something");
+    sort(): number[] {
+        const { length } = this.collection;
+        for(let i=0; i<length; i++) {
+            for(let j=0; j<length-i-1; j++) {
+                if(this.collection[j] > this.collection[j+1]) {
+                    const firstVal = this.collection[j];
+                    this.collection[j] = this.collection[j+1];
+                    this.collection[j+1] = firstVal;
+                }
+            }
+        }
+        return this.collection;
+    }
 }
+
+let array = [10, 3, -5, 0];
+const sorter = new Sorter(array);
+array = sorter.sort();
+console.log(array);
+
+

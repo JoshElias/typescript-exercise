064df3c3431e11b6b90b619438b9540839df1583
Josh Elias <code.5jzcm@simplelogin.com>
2022-10-27 13:38:49 -0400

    add LinkedList

--- sort/src/LinkedList.ts
+++ sort/src/LinkedList.ts
@@ -1,19 +1,90 @@
 import { Sortable } from "./Sorter";
 
-export class NumbersCollection implements Sortable{
+class Node {
+    next: Node | null = null;
+
+    constructor(public data: number) {}
+}
+
+export class LinkedList implements Sortable{
+    head: Node | null = null;
+
     constructor(public data: number[]) {
 
     }
 
-    get length() { return this.data.length; }
+    get length():number { 
+        if(!this.head) return 0;
 
-    compare(firstIndex: number, secondIndex: number): boolean {
-        return this.data[firstIndex] > this.data[secondIndex];
+        let length = 1;
+        let node = this.head;
+        while(node.next) {
+            length++;
+            node = node.next;
+        }
+
+        return length; 
     }
 
-    swap(firstIndex: number, secondIndex: number): void {
-        const firstVal = this.data[firstIndex];
-        this.data[firstIndex] = this.data[secondIndex];
-        this.data[secondIndex] = firstVal;
+    at(i: number): Node {
+        if(!this.head) {
+            throw new Error("Index out of bounds");
+        }
+
+        let counter = 0;
+        let node: Node | null = this.head;
+        while(node) {
+            if(counter == i) {
+                return node;
+            }
+
+            counter++;
+            node = node.next;
+        }
+
+        throw new Error("Index out of bounds");
+    }
+
+    add(data: number): void {
+        const node = new Node(data);
+
+        if(!this.head) {
+            this.head = node;
+            return;
+        }
+
+        let tail = this.head;
+        while(tail.next) {
+            tail = tail.next;
+        }
+
+        tail.next = node;
+    }
+
+    compare(firstI: number, secondI: number): boolean {
+        if(!this.head) {
+            throw new Error("List is empty");
+        }
+
+        return this.at(firstI).data > this.at(secondI).data;
+    }
+
+    swap(firstI: number, secondI: number): void {
+        const firstNode = this.at(firstI);
+        const secondNode = this.at(secondI);
+
+        const firstVal = firstNode.data;
+        firstNode.data = secondNode.data;
+        secondNode.data = firstVal;
+    }
+
+    print(): void {
+        if(!this.head) return;
+
+        let node: Node | null = this.head;
+        while(node) {
+            console.log(node.data);
+            node = node.next;
+        }
     }
 }

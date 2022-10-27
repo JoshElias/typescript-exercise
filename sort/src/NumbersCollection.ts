10a616ae973ce2582b16d2e3ceecd253073611c5
Josh Elias <code.5jzcm@simplelogin.com>
2022-10-27 12:30:20 -0400

    add NumbersCollection

---
+++ sort/src/NumbersCollection.ts
@@ -0,0 +1,17 @@
+export class NumbersCollection {
+    constructor(public data: number[]) {
+
+    }
+
+    get length() { return this.data.length; }
+
+    compare(firstIndex: number, secondIndex: number): boolean {
+        return this.data[firstIndex] > this.data[secondIndex];
+    }
+
+    swap(firstIndex: number, secondIndex: number): void {
+        const firstVal = this.data[firstIndex];
+        this.data[firstIndex] = this.data[secondIndex];
+        this.data[secondIndex] = firstVal;
+    }
+}
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,24 +1,7 @@
-class Sorter {
-    constructor(public collection: number[]) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-    sort(): number[] {
-        const { length } = this.collection;
-        for(let i=0; i<length; i++) {
-            for(let j=0; j<length-i-1; j++) {
-                if(this.collection[j] > this.collection[j+1]) {
-                    const firstVal = this.collection[j];
-                    this.collection[j] = this.collection[j+1];
-                    this.collection[j+1] = firstVal;
-                }
-            }
-        }
-        return this.collection;
-    }
-}
-
-let array = [10, 3, -5, 0];
-const sorter = new Sorter(array);
-array = sorter.sort();
-console.log(array);
-
-
+let numbers:NumbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbers);
+sorter.sort();
+console.log(numbers.data);

3cf22400c3d11a2e517c146d54b39ea0c16e9c56
Josh Elias <code.5jzcm@simplelogin.com>
2022-10-28 11:38:09 -0400

    read Manchester United wins from file

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+import { readFile } from "fs/promises";
+
+const matches = await readFile("football.csv", {
+    encoding: 'utf-8'
+});
+
+const manUnitedWins = matches
+    .split('\n')
+    .map(row => row.split(','))
+    .reduce((wins, match) => {
+        return ((match[1] === 'Man United' && match[5] === "H")
+            || (match[2] === 'Man United' && match[5] === "A")) 
+        ? ++wins : wins;
+    }, 0); 
+
+
+console.log(`Manchester United Wins: ${manUnitedWins}`);
